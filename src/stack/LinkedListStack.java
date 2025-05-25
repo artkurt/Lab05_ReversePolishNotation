1cd83136d529522e62fc5624788d84df3be8eeb3
artkurt <121359226+artkurt@users.noreply.github.com>
2025-05-25 19:48:15 +0200

    Update LinkedListStack.java
    Klasse LinkedListStack wurde implementiert, inklusive eigener Node-Klasse. Methoden wie push, pop, top und isEmpty wurden ohne Verwendung von Java-Bibliotheken manuell mit einer einfach verketteten Liste umgesetzt

--- src/stack/LinkedListStack.java
+++ src/stack/LinkedListStack.java
@@ -1,49 +1,52 @@
 package stack;
 
 public class LinkedListStack<E> implements Stack<E> {
+
 	private class Node<T> {
 		T data;
 		Node<T> next;
-		// implement the nodes here
-		/**
-		 * toString method that matches test cases for easy testing.
-		 */
+
+		Node(T data, Node<T> next) {
+			this.data = data;
+			this.next = next;
+		}
+
 		@Override
 		public String toString() {
-			return ((next == null) ? "" : next.toString()+"-") + data.toString() ;
+			return ((next == null) ? "" : next.toString() + "-") + data.toString();
 		}
 	}
 
-	Node<E> top;
+	Node<E> top = null;
 
 	@Override
 	public boolean isEmpty() {
-		return false;
+		return top == null;
 	}
 
 	@Override
 	public E top() throws Underflow {
-		return null;
+		if (isEmpty()) throw new Underflow();
+		return top.data;
 	}
 
 	@Override
 	public void push(E element) {
-
+		top = new Node<>(element, top);
 	}
 
 	@Override
 	public E pop() throws Underflow {
-		return null;
+		if (isEmpty()) throw new Underflow();
+		E data = top.data;
+		top = top.next;
+		return data;
 	}
 
-	/**
-	 * toString method that matches test cases for easy testing.
-	 * @return
-	 */
 	@Override
 	public String toString() {
 		if (top == null)
 			return "<empty>";
-		return "<stack: "+top.toString()+">";
+		return "<stack: " + top.toString() + ">";
 	}
 }
