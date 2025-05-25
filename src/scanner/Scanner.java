bc45b3e44a71c6956aba5573608f208579b287b0
artkurt <121359226+artkurt@users.noreply.github.com>
2025-05-25 19:53:51 +0200

    Update Scanner.java
    Implementierung der getToken()-Methode, die einen mathematischen Ausdruck in einzelne Tokens aufteilt (inklusive Unterstützung für mehrstellige Zahlen und Operatoren)

--- src/scanner/Scanner.java
+++ src/scanner/Scanner.java
@@ -8,12 +8,38 @@ import java.util.function.Consumer;
 public class Scanner {
 
     String input;
-    public Scanner(String input){
+
+    public Scanner(String input) {
         this.input = input;
     }
 
     public String[] getToken() {
-        return null;
-    }
+        ArrayList<String> tokens = new ArrayList<>();
+        StringBuilder number = new StringBuilder();
 
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+
+            if (Character.isDigit(c)) {
+                number.append(c);
+            } else {
+                if (number.length() > 0) {
+                    tokens.add(number.toString());
+                    number.setLength(0);
+                }
+
+                if (c == ' ') {
+                    continue;
+                } else {
+                    tokens.add(Character.toString(c));
+                }
+            }
+        }
+
+        if (number.length() > 0) {
+            tokens.add(number.toString());
+        }
+
+        return tokens.toArray(new String[0]);
+    }
 }
